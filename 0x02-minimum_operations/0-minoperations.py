95ecf15c4f4ed9c3114a08c5cfc54396966e51a0
creih <iherr.9@gmail.com>
2024-08-09 03:00:46 +0200

    pycodestyle compliant

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,14 @@ def minOperations(n):
     """method to make the calculation of least times"""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
