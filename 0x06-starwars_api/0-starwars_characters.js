d84d7d6c3592317200eaf4702823fd7d84dfbaae
creih <iherr.9@gmail.com>
2024-09-04 02:50:42 -0700

    making the file 0-starwars_characters.js executable

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
+// this dile is meant to display names of characters in specified movie id
 const req = require('request'); // importing request module
 const movId = process.argv[2]; // get 2nd arg as movie id from the command line
 const url = `https://swapi-api.alx-tools.com/api/films/${movId}/`;
