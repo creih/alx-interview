dbcdbb5fa47130946771e41f8bb33e77171120c8
creih <iherr.9@gmail.com>
2024-09-04 11:41:28 +0200

    just reviewed a lot semistandard issuesin previous file version

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,22 +4,22 @@ const req = require('request'); // importing request module
 const movId = process.argv[2]; // get 2nd arg as movie id from the command line
 const url = `https://swapi-api.alx-tools.com/api/films/${movId}/`;
 
-request(url, (error, response, body) => {
-    if (error) {
+req(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach(char => {
+    req(char, (error, response, body) => {
+      if (error) {
         console.error(error);
         return;
-    }
-    const film = JSON.parse(body);
-    const characters = film.characters;
-
-    characters.forEach(char => {
-        request(char, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            const character = JSON.parse(body);
-            console.log(character.name);
-        });
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
+  });
 });
