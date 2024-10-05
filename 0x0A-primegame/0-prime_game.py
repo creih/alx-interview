3b8596f6f5c6bb8599ad14c9e40554313345d5fc
creih <iherr.9@gmail.com>
2024-10-06 01:09:47 +0200

    returned everything as it was

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-"""
-This module defines and implements a prime number guessing algorithm
-for determining the winner.
+""""
+this module defines and implements a prime number guesing algorithm for winner
 """
 
 
 def isWinner(x, nums):
-    """
-    shaka the winner of the prime number guessing game between Maria and Ben
-    """
-    if x <= 0 or any(n < 1 for n in nums):
-        return None
-
+    """ ishaka winner of primus guessing game """
     def sieve_of_eratosthenes(n):
-        """ Returns a list of prime numbers up to n """
         sieve = [True] * (n + 1)
         sieve[0] = sieve[1] = False
         for i in range(2, int(n**0.5) + 1):
@@ -25,16 +18,12 @@ def isWinner(x, nums):
     primes = sieve_of_eratosthenes(max_num)
     maria_wins = 0
     ben_wins = 0
-
     for n in nums:
-        if n == 1:
-            ben_wins += 1
+        prime_count = sum(primes[1:n+1])
+        if prime_count % 2 != 0:
+            maria_wins += 1
         else:
-            prime_count = sum(primes[2:n+1])
-            if prime_count % 2 != 0:
-                maria_wins += 1
-            else:
-                ben_wins += 1
+            ben_wins += 1
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
