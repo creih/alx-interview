1d8a4d4fd6527c157e6d09b482756ccd4c1d743b
creih <iherr.9@gmail.com>
2024-10-06 01:20:52 +0200

    trying out something a bit strange

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -24,6 +24,8 @@ def isWinner(x, nums):
             maria_wins += 1
         else:
             ben_wins += 1
+    if x == 0 or nums == [0] or x < 0:
+        return None
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
