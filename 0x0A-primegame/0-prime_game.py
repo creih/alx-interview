4bb8f2fee16bb6c70cab16d2167f1efc0c78a325
creih <iherr.9@gmail.com>
2024-10-06 00:27:28 +0200

    still fixing edge cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -17,17 +17,21 @@ def isWinner(x, nums):
                 for j in range(i*i, n + 1, i):
                     sieve[j] = False
         return sieve
+
     max_num = max(nums)
+
     primes = sieve_of_eratosthenes(max_num)
 
     maria_wins = 0
     ben_wins = 0
+
     for n in nums:
         prime_count = sum(primes[1:n+1])
         if prime_count % 2 != 0:
             maria_wins += 1
         else:
             ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
